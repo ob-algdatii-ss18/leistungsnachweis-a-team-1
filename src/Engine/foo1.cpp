7734edca32da88417f2dd668f8404356180c95a5
PMmuc <pmasch@hm.edu>
2018-04-20 04:14:48 -0700

    test am alternative...

--- src/Engine/foo1.cpp
+++ src/Engine/foo1.cpp
@@ -8,3 +8,15 @@ int func1() {
 int add(int a, int b) {
 	return a + b;
 }
+
+int complicated(int a) {
+	if (a >= 3) {
+		return 4;
+	}
+	else if (a < 0)
+	{
+		return 1;
+	}
+
+	return 0;
+}
--- test/Engine/EngineTest.cpp
+++ test/Engine/EngineTest.cpp
@@ -2,7 +2,16 @@
 #include <Graph.h>
 
 extern int add(int a, int b);
+extern int complicated(int a);
 
 TEST_F(EngineTest, ByDefaultTrue) {
     ASSERT_THAT(add(1, 2), 3);
+}
+
+TEST_F(EngineTest, Complicated) {
+    ASSERT_TRUE(complicated(3) != 4);
+}
+
+TEST_F(EngineTest, Complicated1) {
+    ASSERT_TRUE(complicated(4) != 4);
 }
